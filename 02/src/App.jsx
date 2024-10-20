2c15268e97b6c11a33464d3c184a5102e6dc7732
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:04:19 -0300

    Add files via upload

---
+++ 02/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Mensagem from './components/Msg' 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frase Inspiradora</h1>
+      <Mensagem />
+    </div>
+  )
+}
+
+export default App
---
+++ 02/src/components/Msg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mensagem = () => {
+  return (
+    <div>
+      <p>A dor coletiva gera união, Obama</p>
+    </div>
+  )
+}
+
+export default Mensagem
