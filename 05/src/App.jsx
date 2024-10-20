7bde19e1408901e0d9375b06ee7fdd8bbf2ef545
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:05:47 -0300

    Add files via upload

---
+++ 05/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Botao from './Botao'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização de Botão</h1>
+      <Botao />
+    </div>
+  )
+}
+
+export default App
---
+++ 05/src/components/botao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Botao = () => {
+  return (
+    <button>
+      Clique Aqui
+    </button>
+  )
+}
+
+export default Botao
