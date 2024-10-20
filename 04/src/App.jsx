756b0ba90f318af7fa8fbed9b8e914d7ca69b64d
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:04:59 -0300

    Add files via upload

---
+++ 04/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Imagem from './components/Img'
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização de Imagem</h1>
+
+      <Imagem />
+    </div>
+  )
+}
+
+export default App
+
---
+++ 04/src/components/Img.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Imagem = () => {
+  return (
+    <div>
+      <h2>Lontra</h2>
+      <p>A lontra é um mamífero semiaquático pertencente à família dos mustelídeos.</p>
+      <img 
+        src="https://th.bing.com/th/id/R.a3166428aca967d4cc91e1fdaa3520e3?rik=sZrrSklhxdFI7A&riu=http%3a%2f%2f1.bp.blogspot.com%2f--JT5TFsrGV8%2fUa9OBz8a5mI%2fAAAAAAAAADg%2fEMHwcHHft6E%2fs1600%2fLontra%2bd.jpg&ehk=%2fpzqqxXqKs5KS0s1ePv9YzduY5Y7%2bWUGtmtbTa9cas0%3d&risl=&pid=ImgRaw&r=0" 
+        alt="Imagem-lontra" 
+        style={{ width: '400px', height: 'auto' }}
+      />
+    </div>
+  )
+}
+
+export default Imagem
