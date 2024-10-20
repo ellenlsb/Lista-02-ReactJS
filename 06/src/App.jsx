8f2c388c05665feff43c29dc217a4ba01f318858
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:06:09 -0300

    Add files via upload

---
+++ 06/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LinkReact from './Link' 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização de Link</h1>
+      <LinkReact />
+    </div>
+  )
+}
+
+export default App
---
+++ 06/src/components/Link.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LinkReact = () => {
+  return (
+    <div>
+      <h2>Aprenda mais sobre React</h2>
+      <a 
+        href="https://reactjs.org" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+      >
+        Acesse o site do ReactJS
+      </a>
+    </div>
+  )
+}
+
+export default LinkReact
