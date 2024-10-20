9cf97f0d265b0a59e11d1703f01ee1d99f285a5b
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:22:50 -0300

    Add files via upload

---
+++ 10/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Fragmento from './components/fragmento'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização com Fragmentos</h1>
+      <Fragmento />
+    </div>
+  )
+}
+
+export default App
---
+++ 10/src/components/fragmento.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Fragmento = () => {
+  return (
+    <React.Fragment>
+      <h3>PROA</h3>
+      <h3>PROPROFISSÃO</h3>
+      <p>Programa de capacitação profissional que tem como objetivo desenvolver habilidades e competências necessárias para o mercado de trabalho no mundo da tecnologia. </p>
+    </React.Fragment>
+  )
+}
+
+export default Fragmento
