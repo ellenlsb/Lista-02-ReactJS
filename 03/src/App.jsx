5b96054ab708c7ed2b106f47f3925c1afea0dc8b
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:04:40 -0300

    Add files via upload

---
+++ 03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListaTarefas from './components/Lista' 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Minha Lista de Tarefas</h1>
+      <ListaTarefas />
+    </div>
+  )
+}
+
+export default App
---
+++ 03/src/components/Lista.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ListaTarefas = () => {
+  return (
+    <div>
+      <h2>Lista de Tarefas</h2>
+      <ol>
+        <li>Curso</li>
+        <li>Academia</li>
+        <li>Faculdade</li>
+      </ol>
+    </div>
+  )
+}
+
+export default ListaTarefas
