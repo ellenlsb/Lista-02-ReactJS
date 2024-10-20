57ab6adee998aa90927b2d4336f460129d5b633b
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-20 20:22:23 -0300

    Add files via upload

---
+++ 09/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DivEstilizada from './components/DivEst' 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renderização de Div com Estilo</h1>
+      <DivEstilizada />
+    </div>
+  )
+}
+
+export default App
---
+++ 09/src/components/DivEst.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DivEstilizada = () => {
+  return (
+    <div style={{
+      backgroundColor: '#C71585',
+      color: 'white',
+      padding: '20px', 
+      margin: '30px', 
+      borderRadius: '10px', 
+      textAlign: 'center' 
+    }}>
+      <h2>PROA 2K24</h2>
+      <p>Demoday SABERAMA</p>
+    </div>
+  )
+}
+
+export default DivEstilizada
